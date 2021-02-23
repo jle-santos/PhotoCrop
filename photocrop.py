fd0e9e1d6b5f0e6d6cfe02d8f089141a7a6fb215
unknown <santos.lemuel@outlook.com>
2021-02-22 21:09:23 -0800

    Edge detection version

--- photocrop.py
+++ photocrop.py
@@ -1,44 +1,67 @@
-# Crops scanned photographs to proper size
-import cv2 as cv
-from datetime import datetime
+import cv2
+import numpy as np
+import imutils
 
-print("PhotoCrop Alpha V0.1 - May 12, 2020")
+image = cv2.imread('../test2.png')
 
-# Open File and show
-photo = cv.imread("Images/test2.png")
+def getTransformation(box, cntr):
 
-imgray = cv.cvtColor(photo, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 254, 255, cv.THRESH_BINARY)
-#cv.imshow("Grayscale", imgray)
-#cv.imshow("Threshold", thresh)
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-cv.drawContours(photo, contours, -1, (0, 0, 0), 3)
+    width = int(cntr[1][0])
+    height = int(cntr[1][1])
 
-def cropPhoto(rawScan, photoData):
-    (centerX, centerY), (width, height), theta = cv.minAreaRect(photoData)
-    (cornerX, cornerY) = (int(centerX-width/2), int(centerY-height/2))
-    print("Leftmost corner coordinates: ", (cornerX, cornerY))
-    croppedPhoto = rawScan[cornerY:cornerY+int(height), cornerX:cornerX+int(width)]
-    return croppedPhoto
+    src_pts = box.astype("int")
+    # coordinate of the points in box points after the rectangle has been
+    # straightened
+    dst_pts = np.array([[0, height - 1],
+                        [0, 0],
+                        [width - 1, 0],
+                        [width - 1, height - 1]], dtype="int")
 
-# Bounding Box
-index = 0
+    # the perspective transformation matrix
+    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
-for imBox in contours:
-    index += 1
-    center, size, theta = cv.minAreaRect(imBox)
-    print(cv.minAreaRect(imBox))
+    return M
 
-    croppedPhoto = cropPhoto(photo, imBox)
-    cv.imshow(str(index), croppedPhoto)
-    now = datetime.now()
-    filename = "Images/img"+str(index)+now.strftime("_%m%d%Y%H%M%S")+".png"
-    cv.imwrite(filename, croppedPhoto)
+def findImages(image):
 
-    # Number the photos in the scan
-    cv.putText(photo, str(index), (int(center[0]),int(center[1])), cv.FONT_HERSHEY_COMPLEX, 1, (0,0,0), 2, cv.LINE_AA)
+    # Images
+    imgList = []
 
-while(cv.waitKey(100)):
-    cv.imshow("Raw", photo)
-    cv.imwrite("Marked.png", photo)
-    #print("Photos found:", len(contours), "contours")
+    # Convert to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Blur image (Low Pass Filter)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    ret, th = cv2.threshold(gray,210,235,1)
+
+    # Edge Detection
+    edged = cv2.Canny(th, 25, 200)
+
+
+    contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Sort contours by size
+    contours = sorted(contours, key = cv2.contourArea, reverse = True)
+
+    for c in contours:
+        # Get area of box
+        box = cv2.minAreaRect(c)
+        box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+        box = np.array(box, dtype="int")
+
+        Area = image.shape[0]*image.shape[1]
+        if Area/10 < cv2.contourArea(box) < Area*2/3:
+            cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+
+            #mtrx = getTransformation(box, c)
+            #warped = cv2.warpPerspective(image, mtrx, int(c[1][0]), int(c[1][1]))
+            #imgList.append(warped)
+
+    return image
+
+foundImages = findImages(image.copy())
+cv2.imshow("Images", foundImages)
+
+cv2.imwrite("markedImg.png", foundImages)
+cv2.waitKey(0)
